9033d0a2451015400f42bafc5f3d7b1166a52086
manosakpujiha <manos.akpujiha@gmail.com>
2023-02-16 00:45:56 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Hangman from "./Hangman";
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Hangman />
+      </div>
+    );
+  }
 }
 
 export default App;
